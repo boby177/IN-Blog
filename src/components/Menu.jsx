0dcb19a4d144ffb0e4d397faaa3793e2353b5d98
boby177 <bomasu37@gmail.com>
2022-11-24 15:13:22 +0700

    configuring the styles and libraries

---
+++ src/components/Menu.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Menu = () => {
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet consectetur, adipisicing elit",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci libero consequatur distinctio sunt aut hic asperiores harum quisquam qui reiciendis!",
+      img: "https://cdn.pixabay.com/photo/2015/03/17/02/01/cubes-677092__480.png",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet consectetur, adipisicing elit",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci libero consequatur distinctio sunt aut hic asperiores harum quisquam qui reiciendis!",
+      img: "https://media.istockphoto.com/id/169940960/photo/asian-elephant.jpg?s=612x612&w=0&k=20&c=QblEPpog7qLeBjzrvAUNweJUEvUo0Z3KKK3MUBa8Bkw=",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci libero consequatur distinctio sunt aut hic asperiores harum quisquam qui reiciendis!",
+      img: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-fries-small:1-3-product-tile-desktop?wid=765&hei=472&dpr=off",
+    },
+  ];
+  return (
+    <div className="menu">
+      <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={post.img} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read more</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
