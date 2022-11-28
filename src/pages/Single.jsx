40086d5a6369330c24c013201393acc7802697a8
boby177 <bomasu37@gmail.com>
2022-11-28 16:51:43 +0700

    updated single data post from database

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,56 +1,71 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Edit from "../imgs/edit.png";
 import Delete from "../imgs/trash.png";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://1.bp.blogspot.com/-y30Tj2wGlxk/X8PWlVe7OfI/AAAAAAAAA0Y/jOANBLZzkC8e7dZ7xJofiW_Bq7f_MwwCQCLcBGAsYHQ/s626/tech-devices-icons-connected-digital-planet-earth_117023-449.jpg"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://yt3.ggpht.com/a/AATXAJxQKukYxK9pjbXPUrzEChNRF-ZVVPQd-4zg2Q=s900-c-k-c0xffffffff-no-rj-mo"
-            alt=""
-          />
+          {post.userImg && <img src={`../upload/${post.userImg}`} alt="" />}
           <div className="info">
-            <span>Boby</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing.</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque magni
-          cupiditate odit est. Eaque facere voluptates, et consectetur molestias
-          dolores libero eos officiis cumque delectus suscipit voluptatibus in
-          explicabo deserunt quos ex obcaecati quibusdam magnam amet provident,
-          ea eveniet corrupti. Non quos quo aliquam magnam cupiditate quisquam
-          necessitatibus blanditiis ullam inventore neque, cum voluptatem
-          accusantium asperiores est voluptatibus incidunt. Corporis assumenda
-          quia debitis alias temporibus, explicabo rem obcaecati dicta sed,
-          deserunt laudantium ex fugiat provident vel quisquam optio quas sunt
-          doloremque! Nemo consequuntur quam neque deleniti repellendus quo
-          minima fugit doloremque libero harum ducimus quasi mollitia
-          accusantium commodi, fuga quisquam vitae praesentium? Non quisquam
-          similique voluptatibus, voluptate nam porro autem ex qui minus quia ad
-          eius sit fugiat beatae modi libero error eveniet doloremque dolores
-          quidem mollitia voluptates necessitatibus. Facilis reprehenderit
-          eaque, cupiditate eveniet id cumque amet vero officia soluta, ut
-          ducimus aliquam sint quasi repudiandae aut inventore perspiciatis
-          illo!
-        </p>
+        <h1>{post.title}</h1>
+        {getText(post.desc)}
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   );
 };
